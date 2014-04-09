708ba78500b539867ba0b4aba708123c1f0d630a
Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
2014-04-09 14:05:10 -0300

    fix license

--- avr-asm-ram-version/ram.c
+++ avr-asm-ram-version/ram.c
@@ -1,3 +1,15 @@
+/* 
+ * Arduino RAM library : interface with 30-pin SIMM RAM modules
+ *
+ * Copyright (C) 2014 Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
+ *
+ *  This ram library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+*/
+
 #include <avr/wdt.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
--- avr-asm-ram-version/ram.h
+++ avr-asm-ram-version/ram.h
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2014 Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
  *
- *  This ram test is free software; you can redistribute it and/or modify
+ *  This ram library is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
